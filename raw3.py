b20ec934f3cd9bdf3f26a9b40a841dee4d453e1a
Laura Aston <laura.aston@monash.edu>
2020-07-23 14:54:59 +1000

    use fancy iterable unpacking

--- raw3.py
+++ raw3.py
@@ -1,10 +1,9 @@
 def compute(expression):
-    values = expression.split(' ')
-    num0 = int(values[0])
-    operator = values[1]
-    num1 = int(values[2])
-    if operator == '+':
-        return num0 + num1
-    else:
-        print('unknown operator!')
-        return None
+  num0, operator, num1 = expression.split(' ‘)
+  if operator == ‘+’:
+    return num0 + num1
+  if operator == ‘-’:
+  return num0 - num1
+  else:
+    print(‘unknown operator!’)
+    return None
